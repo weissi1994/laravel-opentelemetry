8c06d1f29817dc71e60b6819c333238b043aae09
Daniel Weissengruber <daniel.weissengruber@gmail.com>
2022-11-02 23:14:16 +0100

    Create ObservantTrait.php

---
+++ src/ObservantTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SeanHood\LaravelOpenTelemetry;
+
+use SeanHood\LaravelOpenTelemetry\Observers\ModelObserver;
+
+trait ObservantTrait
+{
+    public static function bootObservantTrait()
+    {
+        static::observe(new ModelObserver);
+    }
+}
