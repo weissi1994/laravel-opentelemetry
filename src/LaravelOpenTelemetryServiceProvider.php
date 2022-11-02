30cc6ada9aa4e46ff9e4b23be94f283f08c822cf
Daniel Weissengruber <daniel.weissengruber@gmail.com>
2022-11-02 17:51:30 +0100

    Update LaravelOpenTelemetryServiceProvider.php

--- src/LaravelOpenTelemetryServiceProvider.php
+++ src/LaravelOpenTelemetryServiceProvider.php
@@ -4,21 +4,9 @@ namespace SeanHood\LaravelOpenTelemetry;
 use Illuminate\Support\ServiceProvider;
 
 use OpenTelemetry\Contrib\OtlpHttp\Exporter as OTLPExporter;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\HttpFactory;
-use OpenTelemetry\API\Common\Instrumentation\CachedInstrumentation;
-use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
-use OpenTelemetry\Contrib\Otlp\MetricExporter;
-use OpenTelemetry\SDK\Common\Export\Http\PsrTransportFactory;
-use OpenTelemetry\SDK\Common\Time\ClockFactory;
-use OpenTelemetry\SDK\Metrics\MeterProvider;
-use OpenTelemetry\SDK\Metrics\MetricReader\ExportingReader;
-use OpenTelemetry\SDK\Resource\ResourceInfoFactory;
-use OpenTelemetry\SDK\Sdk;
-use OpenTelemetry\SDK\Trace\Sampler\AlwaysOnSampler;
-use OpenTelemetry\SDK\Trace\Sampler\ParentBased;
-use OpenTelemetry\SDK\Trace\SpanProcessor\BatchSpanProcessorBuilder;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\SDK\Trace\Tracer;
 
 /**
  * LaravelOpenTelemetryServiceProvider injects a configured OpenTelemetry Tracer into
@@ -66,50 +54,16 @@ class LaravelOpenTelemetryServiceProvider extends ServiceProvider
         if(!config('laravel_opentelemetry.enable')) {
             return null;
         }
-    
-        $resource = ResourceInfoFactory::defaultResource();
         
-        $spanExporter = OTLPExporter::fromConnectionString(
+        $exporter = OTLPExporter::fromConnectionString(
             config('laravel_opentelemetry.oltp_endpoint'),
             config('laravel_opentelemetry.service_name')
         );
 
-        $reader = new ExportingReader(
-            new MetricExporter(
-                (new PsrTransportFactory(new Client(), new HttpFactory(), new HttpFactory()))
-                    ->create('http://collector:4318/v1/metrics', 'application/x-protobuf')
-            ),
-            ClockFactory::getDefault()
+        $provider = new TracerProvider(
+          new SimpleSpanProcessor($exporter)
         );
 
-        $meterProvider = MeterProvider::builder()
-            ->setResource($resource)
-            ->addReader($reader)
-            ->build();
-
-        $tracerProvider = TracerProvider::builder()
-            ->addSpanProcessor(
-                (new BatchSpanProcessorBuilder($spanExporter))
-                    ->setMeterProvider($meterProvider)
-                    ->build()
-            )
-            ->setResource($resource)
-            ->setSampler(new ParentBased(new AlwaysOnSampler()))
-            ->build();
-        // $provider = new TracerProvider(
-        //     new BatchSpanProcessor($exporter),
-        //     ClockFactory::getDefault()
-        // );
-        Sdk::builder()
-            ->setTracerProvider($tracerProvider)
-            ->setMeterProvider($meterProvider)
-            ->setPropagator(TraceContextPropagator::getInstance())
-            ->setAutoShutdown(true)
-            ->buildAndRegisterGlobal();
-
-        $instrumentation = new CachedInstrumentation('example');
-        $tracer = $instrumentation->tracer();
-
-        return $tracer; //provider->getTracer('io.opentelemetry.contrib.php');
+        return $provider->getTracer('io.opentelemetry.contrib.php');
     }
 }
